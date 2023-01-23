a1fe78f0decc0c8f5a5d0d8606b5c2e4cd14639c
Phil Cartmell <phil.cartmell@outlook.com>
2023-01-23 05:53:02 +0000

    Moved files

---
+++ src/DTR.Core/Configuration/Models/Module.cs
@@ -0,0 +1,18 @@
+﻿namespace DTR.Core.Configuration.Models
+{
+    public class Module
+    {
+        public Module(string @namespace, string name, string system, string[] versions)
+        {
+            Namespace = @namespace;
+            Name = name;
+            System = system;
+            Versions = versions;
+        }
+
+        public string Namespace { get; set; }
+        public string Name { get; set; }
+        public string System { get; set; }
+        public string[] Versions { get; set; }
+    }
+}
